72e2b9e062989d2e4f86e0efea5694d87391b9c3
MAHMOUD-ELIMBABI <96336520+MAHMOUD-ELIMBABI@users.noreply.github.com>
2022-05-16 18:30:18 +0200

    Add files via upload
    ADDING AUTOSAR LIKE DRIVERS

---
+++ AUTOSAR_LIKE_Drivers/DIO_Driver/Dio_prv.h
@@ -0,0 +1,24 @@
+/*
+ * Dio_prv.h
+ *
+ *  Created on: Mar 4, 2022
+ *      Author: MahmoudElImbabi
+ */
+
+#ifndef MCAL_DIO_DRIVER_DIO_PRV_H_
+#define MCAL_DIO_DRIVER_DIO_PRV_H_
+
+/*MACROS*/
+#define DIO_u8NUMBER_OF_CHANNELS  	31
+#define	DIO_u8PORTA 				0
+#define	DIO_u8PORTB 				1
+#define	DIO_u8PORTC 				2
+#define	DIO_u8PORTD 				3
+#define DIO_u8NUMBER_OF_LEVELS		2
+#define DIO_u8REGISTER_SIZE			8
+#define DIO_u8OUTPUT				1
+#define DIO_u8INPUT					0
+#define DIO_u8NUMBER_OF_PORTS		3
+
+
+#endif /* MCAL_DIO_DRIVER_DIO_PRV_H_ */
